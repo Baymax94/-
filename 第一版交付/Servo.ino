543e9509db35d1286e0ccb96075d5a3745ee3c9e
Baymax <baixuelin12hit@outlook.com>
2019-05-05 12:59:28 +0800

    arduino程序
    基于arduino uno 和 nano

---
+++ 第一版交付/Servo.ino
@@ -0,0 +1,43 @@
+#include <Wire.h>
+#include <Adafruit_PWMServoDriver.h>
+
+Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
+#define SERVOMIN  150
+#define SERVOMAX  600
+
+void setup()
+{
+   pwm.begin();
+
+  pwm.setPWMFreq(50);
+  pwm.setPWM(0,0,350);
+  pwm.setPWM(1,0,320);
+  pwm.setPWM(2,0,450);
+  pwm.setPWM(3,0,140);
+  pinMode(A0, INPUT);
+}
+
+void loop()
+{
+  if (digitalRead(A0) == 1) {
+    for (int i = 200; i <= 350; i = i + (1)) {
+      pwm.setPWM(2,0,i);
+      pwm.setPWM(3,0,(map(i, 200, 350, 350, 200)));
+      delay(10);
+    }
+    for (int i = 350; i >= 200; i = i + (-1)) {
+      pwm.setPWM(2,0,i);
+      pwm.setPWM(3,0,(map(i, 350, 200, 200, 350)));
+      delay(10);
+    }
+
+  } else {
+    pwm.setPWM(0,0,280);
+    pwm.setPWM(1,0,260);
+    pwm.setPWM(2,0,140);
+    pwm.setPWM(3,0,450);
+    delay(100);
+
+  }
+
+}
---
+++ 第一版交付/motor_PS2.ino
@@ -0,0 +1,116 @@
+#include <PS2X_lib.h>
+
+PS2X ps2x;
+void _E5_90_8E_E9_80_80() {
+  digitalWrite(2,HIGH);
+  digitalWrite(3,LOW);
+  digitalWrite(4,LOW);
+  digitalWrite(5,HIGH);
+  digitalWrite(6,LOW);
+  digitalWrite(7,HIGH);
+  digitalWrite(8,HIGH);
+  digitalWrite(9,LOW);
+}
+
+void _E5_B7_A6_E8_BD_AC() {
+  digitalWrite(2,HIGH);
+  digitalWrite(3,LOW);
+  digitalWrite(4,HIGH);
+  digitalWrite(5,LOW);
+  digitalWrite(6,HIGH);
+  digitalWrite(7,LOW);
+  digitalWrite(8,HIGH);
+  digitalWrite(9,LOW);
+}
+
+void _E5_81_9C_E6_AD_A2() {
+  digitalWrite(2,LOW);
+  digitalWrite(3,LOW);
+  digitalWrite(4,LOW);
+  digitalWrite(5,LOW);
+  digitalWrite(6,LOW);
+  digitalWrite(7,LOW);
+  digitalWrite(8,LOW);
+  digitalWrite(9,LOW);
+}
+
+void _E5_89_8D_E8_BF_9B() {
+  digitalWrite(2,LOW);
+  digitalWrite(3,HIGH);
+  digitalWrite(4,HIGH);
+  digitalWrite(5,LOW);
+  digitalWrite(6,HIGH);
+  digitalWrite(7,LOW);
+  digitalWrite(8,LOW);
+  digitalWrite(9,HIGH);
+}
+
+void _E5_8F_B3_E7_A7_BB() {
+  digitalWrite(2,HIGH);
+  digitalWrite(3,LOW);
+  digitalWrite(4,HIGH);
+  digitalWrite(5,LOW);
+  digitalWrite(6,LOW);
+  digitalWrite(7,HIGH);
+  digitalWrite(8,LOW);
+  digitalWrite(9,HIGH);
+}
+
+void _E5_8F_B3_E8_BD_AC() {
+  digitalWrite(2,LOW);
+  digitalWrite(3,HIGH);
+  digitalWrite(4,LOW);
+  digitalWrite(5,HIGH);
+  digitalWrite(6,LOW);
+  digitalWrite(7,HIGH);
+  digitalWrite(8,LOW);
+  digitalWrite(9,HIGH);
+}
+
+void _E5_B7_A6_E7_A7_BB() {
+  digitalWrite(2,LOW);
+  digitalWrite(3,HIGH);
+  digitalWrite(4,LOW);
+  digitalWrite(5,HIGH);
+  digitalWrite(6,HIGH);
+  digitalWrite(7,LOW);
+  digitalWrite(8,HIGH);
+  digitalWrite(9,LOW);
+}
+
+void setup()
+{
+  Serial.begin(57600);
+  ps2x.config_gamepad(12, 11,10,13, false, false);
+  pinMode(2, OUTPUT);
+  pinMode(3, OUTPUT);
+  pinMode(4, OUTPUT);
+  pinMode(5, OUTPUT);
+  pinMode(6, OUTPUT);
+  pinMode(7, OUTPUT);
+  pinMode(8, OUTPUT);
+  pinMode(9, OUTPUT);
+}
+
+void loop()
+{
+  ps2x.read_gamepad(false, 0);if (ps2x.Button(PSB_PAD_UP)) {
+    _E5_89_8D_E8_BF_9B();
+
+  } else if (ps2x.Button(PSB_PAD_DOWN)) {
+    _E5_90_8E_E9_80_80();
+  } else if (ps2x.Button(PSB_PAD_RIGHT)) {
+    _E5_B7_A6_E8_BD_AC();
+  } else if (ps2x.Button(PSB_PAD_LEFT)) {
+    _E5_8F_B3_E8_BD_AC();
+  } else if (ps2x.Button(PSB_SQUARE)) {
+    _E5_B7_A6_E7_A7_BB();
+  } else if (ps2x.Button(PSB_CIRCLE)) {
+    _E5_8F_B3_E7_A7_BB();
+  } else {
+    _E5_81_9C_E6_AD_A2();
+
+  }
+  delay(100);
+
+}
